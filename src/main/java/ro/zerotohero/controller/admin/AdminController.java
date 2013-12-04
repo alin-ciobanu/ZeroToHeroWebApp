d97f676890f5c73646933c401eb8291191c53e29
Alin <alin.ciobanu@cti.pub.ro>
2013-12-04 16:33:22 +0200

    web.xml update

--- src/main/java/ro/zerotohero/controller/admin/AdminController.java
+++ src/main/java/ro/zerotohero/controller/admin/AdminController.java
@@ -1,9 +1,13 @@
 package ro.zerotohero.controller.admin;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import ro.zerotohero.model.AdminEntity;
 
 @Controller
 @RequestMapping("/admin")
@@ -11,6 +15,30 @@ public class AdminController {
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String index(ModelMap model) {
+
+		AdminEntity admin1 = new AdminEntity();
+		admin1.setFirstName("Ion");
+		admin1.setLastName("Ionescu");
+		admin1.setEmail("ion.ionescu@mail.ro");
+		admin1.setPassword("password");
+
+		AdminEntity admin2 = new AdminEntity();
+		admin2.setFirstName("Vasile");
+		admin2.setLastName("Vasilescu");
+		admin2.setEmail("vv@mail.ro");
+		admin2.setPassword("password");
+
+		AdminEntity admin3 = new AdminEntity();
+		admin3.setFirstName("George");
+		admin3.setLastName("Georgescu");
+		admin3.setEmail("gg@mail.ro");
+		admin3.setPassword("password");
+
+		ArrayList<AdminEntity> listaAdmin = new ArrayList<AdminEntity>();
+		listaAdmin.add(admin1);
+		listaAdmin.add(admin2);
+		listaAdmin.add(admin3);
+
 		model.addAttribute("message", "Application Index");
 		return "admin/index";
 	}
--- src/main/java/ro/zerotohero/controller/employee/EmployeeController.java
+++ src/main/java/ro/zerotohero/controller/employee/EmployeeController.java
@@ -1,9 +1,13 @@
 package ro.zerotohero.controller.employee;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import ro.zerotohero.model.EmployeeEntity;
 
 @Controller
 @RequestMapping("/employee")
@@ -11,6 +15,30 @@ public class EmployeeController {
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String index(ModelMap model) {
+
+		EmployeeEntity employee1 = new EmployeeEntity();
+		employee1.setFirstName("Emp1_F");
+		employee1.setLastName("Emp1_L");
+		employee1.setEmail("emp1@mail.ro");
+		employee1.setPassword("password");
+
+		EmployeeEntity employee2 = new EmployeeEntity();
+		employee2.setFirstName("Emp2_F");
+		employee2.setLastName("Emp2_L");
+		employee2.setEmail("emp3@mail.ro");
+		employee2.setPassword("password");
+
+		EmployeeEntity employee3 = new EmployeeEntity();
+		employee3.setFirstName("Emp3_F");
+		employee3.setLastName("Emp3_L");
+		employee3.setEmail("emp3@mail.ro");
+		employee3.setPassword("password");
+
+		ArrayList<EmployeeEntity> listaEmployee = new ArrayList<EmployeeEntity>();
+		listaEmployee.add(employee1);
+		listaEmployee.add(employee2);
+		listaEmployee.add(employee3);
+
 		model.addAttribute("message", "Application Index");
 		return "employee/index";
 	}
