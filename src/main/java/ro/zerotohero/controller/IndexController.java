ab91131b523d0f50fe6094c527108f5f5f0c679e
Alin <alin.ciobanu@cti.pub.ro>
2013-12-04 21:15:46 +0200

    lista de roluri in view-ul "/"

--- src/main/java/ro/zerotohero/controller/IndexController.java
+++ src/main/java/ro/zerotohero/controller/IndexController.java
@@ -1,9 +1,13 @@
 package ro.zerotohero.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import ro.zerotohero.model.RoleEntity;
 
 @Controller
 @RequestMapping("/")
@@ -11,7 +15,26 @@ public class IndexController {
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String index(ModelMap model) {
+
+		RoleEntity role1 = new RoleEntity();
+		role1.setId("1");
+		role1.setName("Role #1");
+
+		RoleEntity role2 = new RoleEntity();
+		role2.setId("2");
+		role2.setName("Role #2");
+
+		RoleEntity role3 = new RoleEntity();
+		role3.setId("3");
+		role3.setName("Role #3");
+
+		ArrayList<RoleEntity> roleList = new ArrayList<RoleEntity>();
+		roleList.add(role1);
+		roleList.add(role2);
+		roleList.add(role3);
+
 		model.addAttribute("message", "Application Index");
+		model.addAttribute("roleList", roleList);
 		return "index";
 	}
 
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,6 +1,28 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <html>
 <body>
 <h2>Hello World!</h2>
 <p>${message}</p>
+
+<table>
+	<thead>
+		<th>
+			Role name
+		</th>
+		<th>
+			ID
+		</th>
+	</thead>
+	<c:forEach var="role" varStatus="status" items="${roleList}">
+		<tr>
+			<td>${role.name}</td>
+			<td>${role.id}</td>
+		</tr>
+	</c:forEach>
+</table>
+
 </body>
 </html>
